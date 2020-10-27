c584c642790c6c43882b597ade9e5dabd0d9925b
sirx16 <atonuslperry@gmail.com>
2020-10-27 11:05:39 -0600

    commit tree 7c9a7c0fa675e6f5027815f73b3e4290183203f7

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
-
+def sort_array_asc(array)
+    array.sort
+ end
+ def sort_array_desc(array)
+   array.sort.reverse
+ end
+ def sort_array_char_count(array)
+   array.sort {|a,b| a.length <=> b.length}
+ end
+ def swap_elements(array)
+   array[1],array[2]=array[2],array[1]
+   return array
+ end
+ def reverse_array(array)
+   array.reverse.each {|v| v}
+ end
+def kesha_maker(array)
+    array.each {|element| element[2] = "$"}     
+end
+def find_a(array)
+    array.select{|element| element[0] =="a"}
+end
+def sum_array(array)
+    array.inject {|add,num| add+num}
+ end
+ def add_s(array)
+     notouch = array[1]
+     copy= array.each_with_index.collect{|element,i| element+"s"}
+     copy[1] = notouch
+     copy
+ end
